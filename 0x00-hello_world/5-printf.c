d07a24ff8f06a9e57cd8b3662c36e997dba882ae
NqobileN <reitumetse.nqobile@gmail.com>
2023-02-16 10:52:09 -0800

    C Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * main - print output using printf function
- * 
  * Return: 0 indicates success
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
