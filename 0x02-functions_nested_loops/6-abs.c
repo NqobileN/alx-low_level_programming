b84c373f2f20588b9f3c901960af1d139d2d3b83
NqobileN <reitumetse.nqobile@gmail.com>
2023-02-21 17:01:00 -0800

    C-Functions Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an interger
+ * @n: n is an interger
+ * Return: Always success
+ */
+
+int _abs(int)
+{
+	if (n > 0)
+	{
+		return (0);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
