27ddaacf5f3bbdffbc8d7394e9187dabdcadef1e
NqobileN <reitumetse.nqobile@gmail.com>
2023-02-21 17:36:01 -0800

    C-Functions Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* add - adds two numbers
+* @num1 : parameter1
+* @num2 : parameter2
+* Return: always success
+*/
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
