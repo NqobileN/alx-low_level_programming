0811501f1c2d1a6498791ae5dd5b2f8b25e06bfd
NqobileN <reitumetse.nqobile@gmail.com>
2023-02-27 08:58:45 -0800

    C-Arrays Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a fuction that takes a pointer into a parameter and updates value
+ * @n: input value
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
